6261c97cdd8526126c5a525ab0ee98396cd4a615
Evan Brown <ezb@google.com>
2021-04-01 09:40:03 -0700

    Use fast division technique based on https://ridiculousfish.com/blog/posts/labor-of-division-episode-i.html and https://lemire.me/blog/2019/02/20/more-fun-with-fast-remainders-when-the-divisor-is-a-constant/.
    We need to store an additional one-byte variable, which increases sizeof(TimeSeriesTracker) by alignof(void*).
    
    PiperOrigin-RevId: 366269301
    Change-Id: If7724bfff0b19abacf5f2f8a64d285c993f75e33

--- tcmalloc/internal/timeseries_tracker.h
+++ tcmalloc/internal/timeseries_tracker.h
@@ -23,6 +23,8 @@
 
 #include "absl/base/internal/cycleclock.h"
 #include "absl/functional/function_ref.h"
+#include "absl/numeric/bits.h"
+#include "absl/numeric/int128.h"
 #include "absl/time/clock.h"
 #include "absl/time/time.h"
 #include "tcmalloc/internal/logging.h"
@@ -53,11 +55,16 @@ class TimeSeriesTracker {
   enum SkipEntriesSetting { kSkipEmptyEntries, kDoNotSkipEmptyEntries };
 
   explicit constexpr TimeSeriesTracker(Clock clock, absl::Duration w)
-      : window_(w),
-        epoch_length_(window_ / kEpochs),
-        epoch_ticks_(static_cast<uint64_t>(
-            absl::ToDoubleSeconds(epoch_length_) * clock.freq())),
-        clock_(clock) {}
+      : window_(w), epoch_length_(window_ / kEpochs), clock_(clock) {
+    // See comment in GetCurrentEpoch().
+    auto d = static_cast<uint64_t>(absl::ToDoubleSeconds(epoch_length_) *
+                                   clock.freq());
+    div_precision_ = 63 + absl::bit_width(d);
+    epoch_ticks_m_ =
+        static_cast<uint64_t>(
+            (static_cast<absl::uint128>(1) << div_precision_) / d) +
+        1;
+  }
 
   bool Report(S val);
 
@@ -87,8 +94,19 @@ class TimeSeriesTracker {
 
   // Returns the current epoch based on the clock.
   int64_t GetCurrentEpoch() {
-    // Ensure division occurs in the unsigned space.
-    return static_cast<uint64_t>(clock_.now()) / epoch_ticks_;
+    // This is equivalent to
+    // `clock_.now() / (absl::ToDoubleSeconds(epoch_length_) * clock_.freq())`.
+    // We basically follow the technique from
+    // https://ridiculousfish.com/blog/posts/labor-of-division-episode-i.html,
+    // except that we use one fewer bit of precision than necessary to always
+    // get the correct answer if the numerator were a 64-bit unsigned number. In
+    // this case, because clock_.now() returns a signed 64-bit number (i.e. max
+    // is <2^63), it shouldn't cause a problem. This way, we don't need to
+    // handle overflow so it's simpler. See also:
+    // https://lemire.me/blog/2019/02/20/more-fun-with-fast-remainders-when-the-divisor-is-a-constant/.
+    return static_cast<int64_t>(static_cast<absl::uint128>(epoch_ticks_m_) *
+                                    clock_.now() >>
+                                div_precision_);
   }
 
   const absl::Duration window_;
@@ -97,7 +115,10 @@ class TimeSeriesTracker {
   T entries_[kEpochs]{};
   size_t last_epoch_{0};
   size_t current_epoch_{0};
-  uint64_t epoch_ticks_;
+  // This is the magic constant from
+  // https://ridiculousfish.com/blog/posts/labor-of-division-episode-i.html.
+  uint64_t epoch_ticks_m_;
+  uint8_t div_precision_;
 
   Clock clock_;
 };
