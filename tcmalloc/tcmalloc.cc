b95dab30d7857e2bc82e8d15770d55fb6b004071
Fedor Korotkiy <dartslon@gmail.com>
2021-09-14 18:20:41 +0300

    Remove dead code

--- tcmalloc/tcmalloc.cc
+++ tcmalloc/tcmalloc.cc
@@ -2210,14 +2210,7 @@ extern "C" void* TCMallocInternalNewArray(size_t size)
     TCMALLOC_ALIAS(TCMallocInternalNew);
 #else
 {
-  void* p = fast_alloc(CppPolicy().WithoutHooks(), size);
-  // We keep this next instruction out of fast_alloc for a reason: when
-  // it's in, and new just calls fast_alloc, the optimizer may fold the
-  // new call into fast_alloc, which messes up our whole section-based
-  // stacktracing (see ABSL_ATTRIBUTE_SECTION, above).  This ensures fast_alloc
-  // isn't the last thing this fn calls, and prevents the folding.
-  MallocHook::InvokeNewHook(p, size);
-  return p;
+  return fast_alloc(CppPolicy().WithoutHooks(), size);
 }
 #endif  // TCMALLOC_ALIAS
 
