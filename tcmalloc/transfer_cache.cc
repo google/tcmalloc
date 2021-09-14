43fe3360f416ed1a2b9469e071eba75ba02900d1
Chris Kennelly <ckennelly@google.com>
2021-09-14 10:07:21 -0700

    Revert: Switch to using ring-based TransferCache by default.
    PiperOrigin-RevId: 396622277
    Change-Id: Ib727143363af6f2ca088e3185ed6ba016e02eaf1

--- tcmalloc/transfer_cache.cc
+++ tcmalloc/transfer_cache.cc
@@ -25,7 +25,6 @@
 #include "tcmalloc/experiment.h"
 #include "tcmalloc/guarded_page_allocator.h"
 #include "tcmalloc/internal/cache_topology.h"
-#include "tcmalloc/internal/environment.h"
 #include "tcmalloc/internal/linked_list.h"
 #include "tcmalloc/internal/logging.h"
 #include "tcmalloc/internal/optimization.h"
@@ -111,29 +110,6 @@ ABSL_MUST_USE_RESULT int
 ShardedTransferCacheManager::BackingTransferCache::RemoveRange(void **batch,
                                                                int n) const {
   return Static::transfer_cache().RemoveRange(size_class_, batch, n);
-}
-
-TransferCacheImplementation TransferCacheManager::ChooseImplementation() {
-  // Prefer ring, if we're forcing it on.
-  if (IsExperimentActive(
-          Experiment::TEST_ONLY_TCMALLOC_RING_BUFFER_TRANSFER_CACHE)) {
-    return TransferCacheImplementation::Ring;
-  }
-
-  // Consider opt-outs
-  const char *e = thread_safe_getenv("TCMALLOC_INTERNAL_TRANSFERCACHE_CONTROL");
-  if (e) {
-    if (e[0] == '0') {
-      return TransferCacheImplementation::Legacy;
-    }
-    if (e[0] == '1') {
-      return TransferCacheImplementation::Ring;
-    }
-    Crash(kCrash, __FILE__, __LINE__, "bad env var", e);
-  }
-
-  // Otherwise, default to ring.
-  return TransferCacheImplementation::Ring;
 }
 
 int TransferCacheManager::DetermineSizeClassToEvict() {
