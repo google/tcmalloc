74e09d1872cd8f91b0d0f3e508c38c40a6b6a101
Bin Du <dubin@google.com>
2021-12-06 11:57:22 -0800

    Set up SampledAllocation struct to track sampled allocations.
    PiperOrigin-RevId: 414505263
    Change-Id: If1a237933ff5d043e3cbd9fb83e1712e66e2d161

---
+++ tcmalloc/sampled_allocation.h
@@ -0,0 +1,45 @@
+// Copyright 2021 The TCMalloc Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef TCMALLOC_SAMPLED_ALLOCATION_H_
+#define TCMALLOC_SAMPLED_ALLOCATION_H_
+
+#include "tcmalloc/internal/logging.h"
+#include "tcmalloc/sampled_allocation_recorder.h"
+
+GOOGLE_MALLOC_SECTION_BEGIN
+namespace tcmalloc {
+namespace tcmalloc_internal {
+
+// Stores information about the sampled allocation.
+struct SampledAllocation : public tcmalloc_internal::Sample<SampledAllocation> {
+  SampledAllocation(const SampledAllocation&) = delete;
+  SampledAllocation& operator=(const SampledAllocation&) = delete;
+
+  SampledAllocation(SampledAllocation&&) = delete;
+  SampledAllocation& operator=(SampledAllocation&&) = delete;
+
+  // Puts the object into a clean state and blocks any readers currently
+  // sampling the object.
+  void PrepareForSampling() ABSL_EXCLUSIVE_LOCKS_REQUIRED(lock);
+
+  // The stack trace of the sampled allocation.
+  StackTrace sampled_stack ABSL_GUARDED_BY(lock) = {};
+};
+
+}  // namespace tcmalloc_internal
+}  // namespace tcmalloc
+GOOGLE_MALLOC_SECTION_END
+
+#endif  // TCMALLOC_SAMPLED_ALLOCATION_H_
