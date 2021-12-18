ad50be478e4d708b2c57c450dc59d59b83d301bd
TCMalloc Team <tcmalloc-eng@google.com>
2021-12-18 14:15:20 -0800

    Internal change
    PiperOrigin-RevId: 417175106
    Change-Id: I55206ed0eb3bbfa8b9e675fb8f4e5c649d13528c

--- tcmalloc/transfer_cache_internals.h
+++ tcmalloc/transfer_cache_internals.h
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef TCMALLOC_TRANSFER_CACHE_INTERNAL_H_
-#define TCMALLOC_TRANSFER_CACHE_INTERNAL_H_
+#ifndef TCMALLOC_TRANSFER_CACHE_INTERNALS_H_
+#define TCMALLOC_TRANSFER_CACHE_INTERNALS_H_
 
 #include <sched.h>
 #include <stddef.h>
@@ -882,4 +882,4 @@ class RingBufferTransferCache {
 }  // namespace tcmalloc::tcmalloc_internal::internal_transfer_cache
 GOOGLE_MALLOC_SECTION_END
 
-#endif  // TCMALLOC_TRANSFER_CACHE_INTERNAL_H_
+#endif  // TCMALLOC_TRANSFER_CACHE_INTERNALS_H_
