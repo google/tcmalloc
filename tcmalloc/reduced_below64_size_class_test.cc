e36314f2572e840d806eeb5c3b7ec543f963ef4e
Evan Brown <ezb@google.com>
2022-05-03 08:29:51 -0700

    Add a test for size_map_internal::IsReducedBelow64SizeClass.
    PiperOrigin-RevId: 446200037
    Change-Id: Ifdc6f9b484132684683e071468fe4659cd754801

--- tcmalloc/reduced_below64_size_class_test.cc
+++ tcmalloc/reduced_below64_size_class_test.cc
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "absl/container/flat_hash_set.h"
 #include "tcmalloc/common.h"
 #include "tcmalloc/experiment.h"
 
@@ -29,6 +29,15 @@ TEST(SizeMap, ReducedBelow64SizeClass) {
     EXPECT_TRUE(
         size_map_internal::IsReducedBelow64SizeClass(size_map.class_to_size(i)))
         << i << " " << size_map.class_to_size(i);
+  }
+}
+
+TEST(SizeMap, IsReducedBelow64SizeClass) {
+  const absl::flat_hash_set<int> allowed = {1, 2, 4, 8, 16, 32, 48, 64};
+  for (size_t i = 1; i <= 10000; ++i) {
+    EXPECT_EQ(size_map_internal::IsReducedBelow64SizeClass(i),
+              allowed.contains(i) || i > 64)
+        << i;
   }
 }
 
