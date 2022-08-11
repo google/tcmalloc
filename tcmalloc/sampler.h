870cd1f0fee005310d2663335226b01b897752a0
Chris Kennelly <ckennelly@google.com>
2022-08-11 10:04:01 -0700

    Remove compound assignment to volatile.
    LLVM generates equivalent code for the fast path (sub <reg>, <mem>) and this
    avoids a build warning on C++20 builds.
    
    Resolves #137.
    
    PiperOrigin-RevId: 466983164
    Change-Id: I7338b3d7b48df3ca1ec07bd03c6f4d0db8d44732

--- tcmalloc/sampler.h
+++ tcmalloc/sampler.h
@@ -230,7 +230,7 @@ Sampler::TryRecordAllocationFast(size_t k) {
     // register. This helps compiler generate slightly more efficient
     // sub <reg>, <mem> instruction for subtraction above.
     volatile ssize_t* ptr = const_cast<volatile ssize_t*>(&bytes_until_sample_);
-    *ptr += k;
+    *ptr = *ptr + k;
     return false;
   }
   return true;
