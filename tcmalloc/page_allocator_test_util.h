34bdb15ee8d2a43ec045b58302bc69660ab873bd
TCMalloc Team <tcmalloc-eng@google.com>
2023-02-10 01:32:10 -0800

    Internal change
    PiperOrigin-RevId: 508596156
    Change-Id: I40236849fd7353be4baa45f8b10714d096ac136a

--- tcmalloc/page_allocator_test_util.h
+++ tcmalloc/page_allocator_test_util.h
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef TCMALLOC_HUGE_PAGE_AWARE_ALLOCATOR_TEST_UTIL_H_
-#define TCMALLOC_HUGE_PAGE_AWARE_ALLOCATOR_TEST_UTIL_H_
+#ifndef TCMALLOC_PAGE_ALLOCATOR_TEST_UTIL_H_
+#define TCMALLOC_PAGE_ALLOCATOR_TEST_UTIL_H_
 
 #include <tuple>
 #include <utility>
@@ -76,4 +76,4 @@ class ExtraRegionFactory : public AddressRegionFactory {
 }  // namespace tcmalloc
 GOOGLE_MALLOC_SECTION_END
 
-#endif  // TCMALLOC_HUGE_PAGE_AWARE_ALLOCATOR_TEST_UTIL_H_
+#endif  // TCMALLOC_PAGE_ALLOCATOR_TEST_UTIL_H_
