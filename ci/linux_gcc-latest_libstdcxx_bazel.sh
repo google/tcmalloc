fdcadcb7d32ff05960defb1fc0390d82939f28d2
Chris Kennelly <ckennelly@google.com>
2023-02-22 10:30:46 -0800

    Update latest Linux container for testing.
    PiperOrigin-RevId: 511533454
    Change-Id: Ib0a596cef7cd711543cbb1282c05f107eda18c8c

--- ci/linux_gcc-latest_libstdcxx_bazel.sh
+++ ci/linux_gcc-latest_libstdcxx_bazel.sh
@@ -36,7 +36,7 @@ if [ -z ${EXCEPTIONS_MODE:-} ]; then
   EXCEPTIONS_MODE="-fno-exceptions -fexceptions"
 fi
 
-readonly DOCKER_CONTAINER="gcr.io/google.com/absl-177019/linux_hybrid-latest:20220217"
+readonly DOCKER_CONTAINER="gcr.io/google.com/absl-177019/linux_hybrid-latest:20230217"
 
 # USE_BAZEL_CACHE=1 only works on Kokoro.
 # Without access to the credentials this won't work.
