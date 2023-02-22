0ff169e2b7289fa5fbcf8e7a8c40dbc26dfd68fa
Chris Kennelly <ckennelly@google.com>
2023-02-22 13:03:06 -0800

    Add test to determine if ARM has a correctly configured kAddressBits.
    kAddressBits may be too large when CONFIG_PGTABLE_LEVELS=3 (#82).
    
    PiperOrigin-RevId: 511575218
    Change-Id: I5001342968266b73fcbfdd7b65884cda3488dde7

--- tcmalloc/internal/config_test.cc
+++ tcmalloc/internal/config_test.cc
@@ -14,21 +14,29 @@
 
 #include "tcmalloc/internal/config.h"
 
+#include <fcntl.h>
+
 #include <algorithm>
 
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "absl/strings/match.h"
+#include "absl/strings/str_cat.h"
+#include "google/protobuf/io/gzip_stream.h"
+#include "google/protobuf/io/zero_copy_stream_impl.h"
+#include "tcmalloc/internal/util.h"
 
 namespace tcmalloc {
 namespace tcmalloc_internal {
 namespace {
 
-#ifdef __x86_64__
 TEST(AddressBits, CpuVirtualBits) {
   // Check that kAddressBits is as least as large as either the number of bits
   // in a pointer or as the number of virtual bits handled by the processor.
   // To be effective this test must be run on each processor model.
   const int kPointerBits = 8 * sizeof(void*);
 
+#ifdef __x86_64__
   // LLVM has a miscompile bug around %rbx, see
   // https://bugs.llvm.org/show_bug.cgi?id=17907
   int ret;
@@ -42,10 +50,49 @@ TEST(AddressBits, CpuVirtualBits) {
       /* clobbers */
       : "rdi", "ecx", "edx");
   const int kImplementedVirtualBits = (ret >> 8) & ((1 << 8) - 1);
-
   ASSERT_GE(kAddressBits, std::min(kImplementedVirtualBits, kPointerBits));
-}
+#elif __aarch64__
+  int fd = signal_safe_open("/proc/config.gz", O_RDONLY);
+  if (fd < 0) {
+    GTEST_SKIP() << "Unable to open kernel config.";
+  }
+
+  google::protobuf::io::FileInputStream fs(fd);
+  google::protobuf::io::GzipInputStream gs(&fs, google::protobuf::io::GzipInputStream::GZIP);
+
+  std::string config;
+  do {
+    const void* buf;
+    int size;
+    if (!gs.Next(&buf, &size)) {
+      break;
+    }
+    if (size < 0) {
+      break;
+    }
+
+    absl::StrAppend(
+        &config, absl::string_view(reinterpret_cast<const char*>(buf), size));
+  } while (true);
+
+  constexpr absl::string_view token = "CONFIG_PGTABLE_LEVELS=";
+  ASSERT_THAT(config, testing::HasSubstr(token));
+  auto position = config.find(token);
+  ASSERT_NE(position, std::string::npos);
+  position += token.size();
+  auto eol = config.find('\n', position);
+  ASSERT_NE(eol, std::string::npos);
+  ASSERT_NE(eol, position);
+  absl::string_view string_levels(&config[position], eol - position);
+  int levels;
+  ASSERT_TRUE(absl::SimpleAtoi(string_levels, &levels)) << string_levels;
+
+  ASSERT_GE(levels, 3);
+  const int kImplementedVirtualBits = 39 + (levels - 3) * 9;
+  ASSERT_EQ(kAddressBits, kImplementedVirtualBits);
+  ASSERT_GE(kAddressBits, std::min(kImplementedVirtualBits, kPointerBits));
 #endif
+}
 
 }  // namespace
 }  // namespace tcmalloc_internal
