4ff136771daea0c5cb8499903801c7023c948ca8
Mark Benvenuto <mark.benvenuto@mongodb.com>
2024-03-20 18:34:00 +0000

    Fixes for gcc

--- tcmalloc/internal/percpu_tcmalloc.h
+++ tcmalloc/internal/percpu_tcmalloc.h
@@ -686,8 +686,9 @@ static inline ABSL_ATTRIBUTE_ALWAYS_INLINE bool TcmallocSlab_Internal_Push(
         [cached_slabs_mask] "r"(TCMALLOC_CACHED_SLABS_MASK),
 #endif
         [size_class_lsl2] "r"(size_class << 2), [item] "r"(item)
-      : TCMALLOC_RSEQ_CLOBBER, "cc", "memory"
+      : TCMALLOC_RSEQ_CLOBBER, "memory"
 #if TCMALLOC_INTERNAL_PERCPU_USE_RSEQ_ASM_GOTO_OUTPUT
+      , "cc"
       : overflow_label
 #endif
   );
@@ -881,8 +882,9 @@ inline ABSL_ATTRIBUTE_ALWAYS_INLINE void* TcmallocSlab::Pop(size_t size_class) {
 #endif
         [begin_mark_mask] "n"(kBeginMark), [size_class] "r"(size_class),
         [size_class_lsl2] "r"(size_class << 2)
-      : TCMALLOC_RSEQ_CLOBBER, "cc", "memory"
+      : TCMALLOC_RSEQ_CLOBBER, "memory"
 #if TCMALLOC_INTERNAL_PERCPU_USE_RSEQ_ASM_GOTO_OUTPUT
+      , "cc"
       : underflow_path
 #endif
   );
