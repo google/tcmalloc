8a4bc25e36e93196f9a22ea30092b0666c8eeeed
Marco Elver <elver@google.com>
2024-06-10 10:34:33 -0700

    GPA: Add TrySample benchmark
    Add a benchmark that exclusively benchmarks the guarded sampling-decision
    overhead (excluding the overhead of making a guarded allocation). Since the
    decision to guard may change, the only way to fairly compare differences in the
    sampling decision algorithm is to exclude the overhead of allocations.
    
    The benchmark simply exhausts the pool before the profiled portion of the code.
    
    PiperOrigin-RevId: 641949319
    Change-Id: I2a7159cf367b9565c9847c8a36a3fe24de9cfd17

--- tcmalloc/guarded_page_allocator_benchmark.cc
+++ tcmalloc/guarded_page_allocator_benchmark.cc
@@ -12,42 +12,71 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <unistd.h>
-
 #include <algorithm>
+#include <atomic>
 #include <cstddef>
+#include <functional>
+#include <memory>
+#include <vector>
 
 #include "benchmark/benchmark.h"
 #include "tcmalloc/common.h"
 #include "tcmalloc/guarded_page_allocator.h"
-#include "tcmalloc/internal/allocation_guard.h"
 #include "tcmalloc/internal/config.h"
 #include "tcmalloc/internal/logging.h"
 #include "tcmalloc/internal/page_size.h"
+#include "tcmalloc/malloc_extension.h"
+#include "tcmalloc/pages.h"
+#include "tcmalloc/static_vars.h"
 
 GOOGLE_MALLOC_SECTION_BEGIN
 namespace tcmalloc {
 namespace tcmalloc_internal {
 namespace {
+using GuardedStatus = Profile::Sample::GuardedStatus;
 
-static constexpr size_t kMaxGpaPages = GuardedPageAllocator::kGpaMaxPages;
+constexpr size_t kMaxGpaPages = GuardedPageAllocator::kGpaMaxPages;
 
-// Size of pages used by GuardedPageAllocator.
-static size_t PageSize() {
+// Size of pages used by GuardedPageAllocator. See GuardedPageAllocator::Init().
+size_t GetGpaPageSize() {
   static const size_t page_size =
       std::max(kPageSize, static_cast<size_t>(GetPageSize()));
   return page_size;
 }
 
-void BM_AllocDealloc(benchmark::State& state) {
-  static GuardedPageAllocator* gpa = []() {
+inline auto& GetStackTrace(size_t first_frame) {
+  thread_local StackTrace* s = new StackTrace;
+  s->stack[0] = reinterpret_cast<void*>(first_frame + 42);
+  s->depth = 1;
+  return *s;
+}
+
+std::unique_ptr<GuardedPageAllocator, void (*)(GuardedPageAllocator*)>
+GetGuardedPageAllocator() {
+  static GuardedPageAllocator* gpa = [] {
     auto gpa = new GuardedPageAllocator;
     PageHeapSpinLockHolder l;
     gpa->Init(kMaxGpaPages, kMaxGpaPages);
     gpa->AllowAllocations();
+    // Benchmark should always sample.
+    MallocExtension::SetProfileSamplingRate(1);
+    MallocExtension::SetGuardedSamplingRate(1);
     return gpa;
   }();
-  size_t alloc_size = state.range(0);
+  return {gpa, +[](GuardedPageAllocator* gpa) {
+            // We can't reset GuardedPageAllocator before the benchmark in
+            // multi-threaded mode as it might race with pre-initialization and
+            // concurrent Reset() from all other benchmark threads. Instead,
+            // just reset after each benchmark.
+            gpa->Reset();
+          }};
+}
+
+// Benchmark for guarded page allocation overhead only, to focus on free slot
+// selection, mprotect() overhead, and allocation metadata storage.
+void BM_AllocDealloc(benchmark::State& state) {
+  const size_t alloc_size = state.range(0);
+  auto gpa = GetGuardedPageAllocator();
   for (auto _ : state) {
     char* ptr = reinterpret_cast<char*>(gpa->Allocate(alloc_size, 0).alloc);
     TC_CHECK_NE(ptr, nullptr);
@@ -57,8 +86,94 @@ void BM_AllocDealloc(benchmark::State& state) {
   }
 }
 
-BENCHMARK(BM_AllocDealloc)->Range(1, PageSize());
+BENCHMARK(BM_AllocDealloc)->Range(1, GetGpaPageSize());
 BENCHMARK(BM_AllocDealloc)->Arg(1)->ThreadRange(1, kMaxGpaPages);
+
+auto& GetReserved() {
+  static auto* ret =
+      new std::vector<std::unique_ptr<void, std::function<void(void*)>>>;
+  return *ret;
+}
+
+// Exhaust the pool first so we do not profile allocation overhead.
+void ReservePool(const benchmark::State&) {
+  TC_CHECK(GetReserved().empty());
+  auto* gpa = GetGuardedPageAllocator().release();  // do not Reset()
+  auto deleter = [gpa](void* p) { gpa->Deallocate(p); };
+
+  for (size_t stack_idx = 0;;) {
+    auto alloc = gpa->TrySample(1, 0, Length(1), GetStackTrace(stack_idx));
+    switch (alloc.status) {
+      case GuardedStatus::NoAvailableSlots:
+        TC_CHECK(!GetReserved().empty());
+        return;
+      case GuardedStatus::RateLimited:
+        // Emulate that non-guarded sampling happened.
+        tc_globals.total_sampled_count_.Add(1);
+        break;
+      case GuardedStatus::Filtered:
+        // The filter is rejecting the stack trace, give it one more unique
+        // stack trace.
+        stack_idx++;
+        break;
+      default:
+        if (alloc.alloc) GetReserved().emplace_back(alloc.alloc, deleter);
+        break;
+    }
+  }
+}
+
+void ReleasePool(const benchmark::State&) {
+  TC_CHECK(!GetReserved().empty());
+  GetReserved().clear();
+}
+
+// Benchmark that includes sampling-decision overhead.
+void BM_TrySample(benchmark::State& state) {
+  TC_CHECK(!GetReserved().empty());
+  // Shared between benchmark threads. We don't care if one of the threads
+  // doesn't see one of the statuses.
+  static std::atomic<bool> seen_filtered;
+  seen_filtered = false;
+
+  const size_t alloc_size = state.range(0);
+  auto gpa = GetGuardedPageAllocator();
+  size_t stack_idx = 0;
+
+  for (auto _ : state) {
+    StackTrace& stack_trace = GetStackTrace(stack_idx++ % GetReserved().size());
+    auto alloc = gpa->TrySample(alloc_size, 0, Length(1), stack_trace);
+
+    switch (alloc.status) {
+      case GuardedStatus::RateLimited:
+        tc_globals.total_sampled_count_.Add(1);
+        break;
+      case GuardedStatus::Filtered:
+        seen_filtered.store(true, std::memory_order_relaxed);
+        break;
+      default:
+        TC_CHECK_NE(alloc.status, GuardedStatus::Guarded);
+        TC_CHECK_NE(alloc.status, GuardedStatus::LargerThanOnePage);
+        TC_CHECK_NE(alloc.status, GuardedStatus::Disabled);
+        TC_CHECK_NE(alloc.status, GuardedStatus::MProtectFailed);
+        TC_CHECK_NE(alloc.status, GuardedStatus::TooSmall);
+        break;
+    }
+    TC_CHECK_EQ(alloc.alloc, nullptr);
+  }
+
+  if (state.iterations() > 1000) TC_CHECK(seen_filtered);
+}
+
+BENCHMARK(BM_TrySample)
+    ->Range(1, GetGpaPageSize())
+    ->Setup(ReservePool)
+    ->Teardown(ReleasePool);
+BENCHMARK(BM_TrySample)
+    ->Arg(1)
+    ->ThreadRange(1, kMaxGpaPages)
+    ->Setup(ReservePool)
+    ->Teardown(ReleasePool);
 
 }  // namespace
 }  // namespace tcmalloc_internal
