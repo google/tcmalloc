google/tcmalloc

commit 46d6f3d25eb1fb8445ba4cca707922a9746ff4a6

Release pageheap_lock when releasing from regions.
This requires we mark the pages as used temporarily to prevent them from being
allocated while we are making our system call.

PiperOrigin-RevId: 629463121
Change-Id: I778134e7c32ab1de7873e3ac4ba1e49902387d7f

tcmalloc/BUILD
@@ -911,6 +911,7 @@ cc_test(
         "//tcmalloc/internal:logging",
         "@com_google_absl//absl/memory",
         "@com_google_absl//absl/random",
+        "@com_google_absl//absl/synchronization",
         "@com_google_absl//absl/time",
         "@com_google_googletest//:gtest_main",
     ],

tcmalloc/huge_region_test.cc
@@ -22,6 +22,7 @@
 #include <cstring>
 #include <memory>
 #include <string>
+#include <thread>
 #include <utility>
 #include <vector>
 
@@ -29,6 +30,8 @@
 #include "gtest/gtest.h"
 #include "absl/memory/memory.h"
 #include "absl/random/random.h"
+#include "absl/synchronization/mutex.h"
+#include "absl/synchronization/notification.h"
 #include "absl/time/time.h"
 #include "tcmalloc/common.h"
 #include "tcmalloc/huge_cache.h"
@@ -56,8 +59,6 @@ class HugeRegionTest : public ::testing::Test {
     // we usually don't care about backing calls, unless testing that
     // specifically.
   }
-
-  ~HugeRegionTest() override { mock_.reset(nullptr); }
 
   class MockBackingInterface : public MemoryModifyFunction {
    public:
@@ -316,6 +317,79 @@ TEST_F(HugeRegionTest, Reback) {
     DeleteUnback(allocs[3]);
     CheckMock();
   }
+}
+
+class BlockingRelease final : public MemoryModifyFunction {
+ public:
+  BlockingRelease(absl::Mutex* mu, absl::Notification* notify)
+      : mu_(mu), notify_(notify) {}
+
+  bool operator()(PageId p, Length len) override {
+    if (notify_) {
+      notify_->Notify();
+      notify_ = nullptr;
+    }
+
+    mu_->Lock();
+    mu_->Unlock();
+
+    regions_.emplace_back(p, len);
+
+    return true;
+  }
+
+  std::vector<std::pair<PageId, Length>> regions() const { return regions_; }
+
+ private:
+  absl::Mutex* mu_;
+  absl::Notification* notify_;
+  std::vector<std::pair<PageId, Length>> regions_;
+};
+
+TEST_F(HugeRegionTest, ParallelRelease) {
+  absl::Mutex mu;
+  absl::Notification notify;
+
+  BlockingRelease blocking(&mu, &notify);
+  region_.~HugeRegion();
+  new (&region_) HugeRegion({p_, region_.size()}, blocking);
+
+  auto a1 = Allocate(5 * kPagesPerHugePage / 2);
+  auto a2 = Allocate(5 * kPagesPerHugePage / 2);
+  auto a3 = Allocate(3 * kPagesPerHugePage);
+  auto a4 = Allocate((region_.size() - NHugePages(8)).in_pages());
+
+  Delete(a2);
+  Delete(a4);
+
+  mu.Lock();
+
+  HugeLength released;
+  std::thread t(
+      [&]() { released = region_.Release(region_.size().in_pages()); });
+
+  notify.WaitForNotification();
+
+  // Release has started.  We shouldn't intersect with what is released.
+  auto a5 = Allocate(kPagesPerHugePage);
+
+  // Allow release to procede and finish.
+  mu.Unlock();
+  t.join();
+
+  // a1 rounded up (3), a3 (3), and in-flight alloc a5 (1) are not released.
+  EXPECT_EQ(released, region_.size() - NHugePages(7));
+
+  // Check for intersection.
+  auto regions = blocking.regions();
+  for (const auto& region : regions) {
+    EXPECT_TRUE(a5.p + a5.n <= region.first ||
+                region.first + region.second <= a5.p);
+  }
+
+  Delete(a1);
+  Delete(a3);
+  Delete(a5);
 }
 
 TEST_F(HugeRegionTest, Stats) {

tcmalloc/huge_page_aware_allocator.h
@@ -683,7 +683,7 @@ inline bool HugePageAwareAllocator<Forwarder>::AddRegion() {
   HugeRange r = alloc_.Get(HugeRegion::size());
   if (!r.valid()) return false;
   HugeRegion* region = region_allocator_.New();
-  new (region) HugeRegion(r, unback_);
+  new (region) HugeRegion(r, unback_without_lock_);
   regions_.Contribute(region);
   return true;
 }

tcmalloc/huge_region.h
@@ -534,17 +534,29 @@ inline HugeLength HugeRegion::UnbackHugepages(
   HugeLength released = NHugePages(0);
   size_t i = 0;
   while (i < kNumHugePages) {
-    if (!should_unback[i]) {
+    if (!should_unback[i] || pages_used_[i] > Length(0)) {
       i++;
       continue;
     }
     size_t j = i;
-    while (j < kNumHugePages && should_unback[j]) {
+    while (j < kNumHugePages && should_unback[j] &&
+           pages_used_[j] == Length(0)) {
+      // We toggle the pages as in-use via tracker_, but in debug builds, we
+      // also mark the pages as used too.
+#ifndef NDEBUG
+      pages_used_[j] = kPagesPerHugePage;
+#endif  // !NDEBUG
       j++;
     }
 
     HugeLength hl = NHugePages(j - i);
     HugePage p = location_.start() + NHugePages(i);
+
+    // Toggle the range as in-use, since we will be releasing the page heap
+    // lock.  Marking the pages as used keeps us from allocating them while we
+    // are releasing them.
+    tracker_.Mark(NHugePages(i).in_pages().raw_num(), hl.in_pages().raw_num());
+
     if (ABSL_PREDICT_TRUE(unback_(p.first_page(), hl.in_pages()))) {
       nbacked_ -= hl;
       total_unbacked_ += hl;
@@ -556,6 +568,15 @@ inline HugeLength HugeRegion::UnbackHugepages(
 
       released += hl;
     }
+
+    tracker_.Unmark(NHugePages(i).in_pages().raw_num(),
+                    hl.in_pages().raw_num());
+#ifndef NDEBUG
+    for (size_t k = i; k < j; ++k) {
+      pages_used_[k] = Length(0);
+    }
+#endif  // !NDEBUG
+
     i = j;
   }
 

tcmalloc/internal/range_tracker.h
@@ -117,6 +117,9 @@ class RangeTracker {
   // best fit.
   size_t FindAndMark(size_t n);
 
+  // REQUIRES: the range [index, index + n) is fully unmarked.
+  void Mark(size_t index, size_t n);
+
   // REQUIRES: the range [index, index + n) is fully marked, and
   // was the returned value from a call to FindAndMark.
   // Unmarks it.
@@ -235,6 +238,29 @@ inline size_t RangeTracker<N>::FindAndMark(size_t n) {
   nused_ += n;
   nallocs_++;
   return best_index;
+}
+
+// REQUIRES: the range [index, index + n) is fully marked.
+// Unmarks it.
+template <size_t N>
+inline void RangeTracker<N>::Mark(size_t index, size_t n) {
+  TC_ASSERT(bits_.FindSet(index) >= index + n);
+  bits_.SetRange(index, n);
+  nused_ += n;
+  nallocs_++;
+
+  size_t longest_len = 0;
+  size_t scan_index = 0, scan_len;
+
+  while (bits_.NextFreeRange(scan_index, &scan_index, &scan_len)) {
+    if (scan_len > longest_len) {
+      longest_len = scan_len;
+    }
+
+    scan_index += scan_len;
+  }
+
+  longest_free_ = longest_len;
 }
 
 // REQUIRES: the range [index, index + n) is fully marked.

tcmalloc/internal/range_tracker_test.cc
@@ -289,6 +289,17 @@ TEST_F(RangeTrackerTest, Trivial) {
   EXPECT_THAT(FreeRanges(), ElementsAre(Pair(0, 300)));
 }
 
+TEST_F(RangeTrackerTest, Mark) {
+  EXPECT_EQ(kBits, range_.longest_free());
+  range_.Mark(100, 100);
+  EXPECT_EQ(100, range_.used());
+  EXPECT_EQ(kBits - 200, range_.longest_free());
+  EXPECT_THAT(FreeRanges(), ElementsAre(Pair(0, 100), Pair(200, kBits - 200)));
+  range_.Unmark(100, 100);
+  EXPECT_EQ(0, range_.used());
+  EXPECT_EQ(kBits, range_.longest_free());
+}
+
 }  // namespace
 }  // namespace tcmalloc_internal
 }  // namespace tcmalloc

commit 583996da319881807fd922d12053b668a80c0e05

Add multi-threaded stress test for HugeRegion.
This fails with 46d6f3d25eb1fb8445ba4cca707922a9746ff4a6 when unlocking is
simulated.

PiperOrigin-RevId: 643414546
Change-Id: I835907eab79696fc4d1eff8d955d3a21cc1a7ca9

tcmalloc/BUILD
@@ -947,9 +947,14 @@ cc_test(
         ":common_8k_pages",
         "//tcmalloc/internal:clock",
         "//tcmalloc/internal:logging",
+        "//tcmalloc/testing:thread_manager",
+        "@com_google_absl//absl/base:core_headers",
+        "@com_google_absl//absl/container:fixed_array",
         "@com_google_absl//absl/memory",
         "@com_google_absl//absl/random",
+        "@com_google_absl//absl/synchronization",
         "@com_google_absl//absl/time",
+        "@com_google_absl//absl/types:span",
         "@com_google_googletest//:gtest_main",
     ],
 )

tcmalloc/huge_region_test.cc
@@ -19,6 +19,7 @@
 #include <stdio.h>
 
 #include <algorithm>
+#include <atomic>
 #include <cstring>
 #include <memory>
 #include <string>
@@ -27,9 +28,14 @@
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "absl/base/thread_annotations.h"
+#include "absl/container/fixed_array.h"
 #include "absl/memory/memory.h"
 #include "absl/random/random.h"
+#include "absl/synchronization/mutex.h"
+#include "absl/time/clock.h"
 #include "absl/time/time.h"
+#include "absl/types/span.h"
 #include "tcmalloc/common.h"
 #include "tcmalloc/huge_cache.h"
 #include "tcmalloc/huge_page_subrelease.h"
@@ -38,6 +44,7 @@
 #include "tcmalloc/internal/logging.h"
 #include "tcmalloc/pages.h"
 #include "tcmalloc/stats.h"
+#include "tcmalloc/testing/thread_manager.h"
 
 namespace tcmalloc {
 namespace tcmalloc_internal {
@@ -316,6 +323,143 @@ TEST_F(HugeRegionTest, Reback) {
     DeleteUnback(allocs[3]);
     CheckMock();
   }
+}
+
+class MemorySimulation final : public MemoryModifyFunction {
+ public:
+  MemorySimulation(absl::Mutex& mu, PageId base,
+                   absl::Span<std::atomic<char>> bytes)
+      : mu_(mu), base_(base), bytes_(bytes) {}
+
+  bool operator()(PageId p,
+                  Length len) override ABSL_NO_THREAD_SAFETY_ANALYSIS {
+    // TODO(b/73749855): Simulate with unlocking.
+    mu_.AssertHeld();
+
+    size_t index = (p - base_).raw_num();
+    for (size_t i = 0, n = len.raw_num(); i < n; ++i) {
+      bytes_[index + i].store(0, std::memory_order_release);
+    }
+
+    return true;
+  }
+
+ private:
+  absl::Mutex& mu_;
+  PageId base_;
+  absl::Span<std::atomic<char>> bytes_;
+};
+
+TEST_F(HugeRegionTest, ReleaseFuzz) {
+  absl::Mutex mu;
+  absl::FixedArray<std::atomic<char>> bytes(
+      region_.size().in_pages().raw_num());
+
+  MemorySimulation simulation(mu, p_.first_page(), absl::MakeSpan(bytes));
+
+  region_.~HugeRegion();
+  new (&region_) HugeRegion({p_, region_.size()}, simulation);
+
+  const int kThreads = 10;
+  std::vector<absl::BitGen> rngs(kThreads);
+
+  absl::Mutex state_mu;
+  struct FuzzAlloc {
+    int tid;
+    PageId start;
+    Length len;
+  };
+  std::vector<FuzzAlloc> allocs;
+
+  ThreadManager threads;
+  threads.Start(kThreads, [&](int tid) {
+    switch (absl::Uniform(rngs[tid], 0, 4)) {
+      case 0: {
+        const size_t n =
+            absl::Uniform(rngs[tid], 1u, region_.size().in_pages().raw_num());
+
+        FuzzAlloc f;
+        f.tid = tid;
+        f.len = Length(n);
+        bool from_released;
+        {
+          absl::MutexLock l(&mu);
+          if (!region_.MaybeGet(f.len, &f.start, &from_released)) {
+            break;
+          }
+        }
+
+        const size_t base = (f.start - p_.first_page()).raw_num();
+        for (size_t i = 0; i < n; ++i) {
+          const int old_val =
+              bytes[base + i].exchange(tid, std::memory_order_acq_rel);
+          TC_CHECK_EQ(old_val, 0);
+        }
+
+        {
+          absl::MutexLock l(&state_mu);
+          allocs.push_back(f);
+        }
+        break;
+      }
+      case 1: {
+        FuzzAlloc f;
+
+        {
+          absl::MutexLock l(&state_mu);
+          if (allocs.empty()) {
+            break;
+          }
+
+          const size_t index = absl::Uniform(rngs[tid], 0u, allocs.size());
+          f = allocs[index];
+          std::swap(allocs[index], allocs.back());
+          allocs.resize(allocs.size() - 1);
+        }
+
+        const size_t base = (f.start - p_.first_page()).raw_num();
+        for (size_t i = 0; i < f.len.raw_num(); ++i) {
+          const int old_val =
+              bytes[base + i].exchange(0, std::memory_order_acq_rel);
+          TC_CHECK_EQ(old_val, f.tid);
+        }
+
+        absl::MutexLock l(&mu);
+        region_.Put(f.start, f.len, false);
+        break;
+      }
+      case 2: {
+        absl::MutexLock l(&state_mu);
+        if (allocs.empty()) {
+          break;
+        }
+
+        const size_t index = absl::Uniform(rngs[tid], 0u, allocs.size());
+        FuzzAlloc f = allocs[index];
+
+        const size_t base = (f.start - p_.first_page()).raw_num();
+        for (size_t i = 0; i < f.len.raw_num(); ++i) {
+          const int val = bytes[base + i].load(std::memory_order_acquire);
+          TC_CHECK_EQ(val, f.tid);
+        }
+
+        break;
+      }
+      case 3: {
+        const Length to_release = Length(
+            absl::Uniform(rngs[tid], 0u, region_.size().in_pages().raw_num()));
+
+        absl::MutexLock l(&mu);
+        region_.Release(to_release);
+
+        break;
+      }
+    }
+  });
+
+  absl::SleepFor(absl::Seconds(1));
+
+  threads.Stop();
 }
 
 TEST_F(HugeRegionTest, Stats) {