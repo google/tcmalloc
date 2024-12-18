91765c11461a01579fcbdddf430a556b818818c4
TCMalloc Team <tcmalloc-eng@google.com>
2024-12-18 00:46:47 -0800

    No public description
    PiperOrigin-RevId: 707436670
    Change-Id: Ifc653308cd37fb521ecdb220b1c005291486b1e0

--- tcmalloc/internal/residency_test.cc
+++ tcmalloc/internal/residency_test.cc
@@ -34,7 +34,6 @@
 #include "absl/log/check.h"
 #include "absl/status/status.h"
 #include "absl/strings/str_cat.h"
-#include "absl/strings/str_format.h"
 #include "absl/strings/string_view.h"
 #include "tcmalloc/internal/allocation_guard.h"
 #include "tcmalloc/internal/page_size.h"
