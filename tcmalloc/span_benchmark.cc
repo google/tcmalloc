e2275b91168530cb97385363ff2db6cad371a199
Chris Kennelly <ckennelly@google.com>
2025-06-18 09:04:49 -0700

    Handle empty size classes in microbenchmark.
    PiperOrigin-RevId: 772942558
    Change-Id: I6704333b43783835b7681500fbca52dfdc735fe9

--- tcmalloc/span_benchmark.cc
+++ tcmalloc/span_benchmark.cc
@@ -70,6 +70,10 @@ void BM_single_span(benchmark::State& state) {
   const int size_class = state.range(0);
 
   size_t size = tc_globals.sizemap().class_to_size(size_class);
+  if (size == 0) {
+    state.SkipWithMessage("Empty size class");
+    return;
+  }
   uint32_t reciprocal = Span::CalcReciprocal(size);
   size_t batch_size = tc_globals.sizemap().num_objects_to_move(size_class);
   RawSpan raw_span;
@@ -97,8 +101,11 @@ void BM_single_span_fulldrain(benchmark::State& state) {
   const int size_class = state.range(0);
 
   size_t size = tc_globals.sizemap().class_to_size(size_class);
+  if (size == 0) {
+    state.SkipWithMessage("Empty size class");
+    return;
+  }
   uint32_t reciprocal = Span::CalcReciprocal(size);
-  TC_CHECK_GT(size, 0);
   size_t npages = tc_globals.sizemap().class_to_pages(size_class);
   size_t batch_size = tc_globals.sizemap().num_objects_to_move(size_class);
   size_t objects_per_span = npages * kPageSize / size;
