eb4e7c035d2b3b50a76a6ad0f84785e0932e9fc1
Chris Kennelly <ckennelly@google.com>
2025-07-01 12:08:29 -0700

    Bolster testing for hooks.
    PiperOrigin-RevId: 778138490
    Change-Id: I0894b9082f2a90a42f0df59db79c6dba2581b746

--- tcmalloc/testing/hooks_test.cc
+++ tcmalloc/testing/hooks_test.cc
@@ -25,6 +25,7 @@
 #include <string>
 #include <vector>
 
+#include "benchmark/benchmark.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "absl/debugging/leak_check.h"
@@ -32,15 +33,283 @@
 #include "tcmalloc/internal/logging.h"
 #include "tcmalloc/malloc_extension.h"
 #include "tcmalloc/malloc_hook.h"
+#include "tcmalloc/malloc_hook_invoke.h"
+#include "tcmalloc/testing/malloc_hook_recorder.h"
+#include "tcmalloc/testing/testutil.h"
 
 namespace tcmalloc {
 namespace {
 
+using tcmalloc_testing::MallocHookRecorder;
 using testing::Contains;
 using testing::ElementsAre;
 using testing::HasSubstr;
 using testing::Lt;
 using testing::SizeIs;
+
+class TCMallocHookTest : public testing::Test {
+ public:
+  using Type = MallocHookRecorder::Type;
+
+  MallocHookRecorder::CallEntry NewCall(
+      const void* ptr, size_t requested_size, size_t allocated_size,
+      HookMemoryMutable is_mutable = HookMemoryMutable::kMutable) {
+    return {
+        MallocHookRecorder::kNew, ptr,        requested_size,
+        allocated_size,           is_mutable, MallocHookRecorder::kTCMalloc};
+  }
+  MallocHookRecorder::CallEntry DeleteCall(
+      const void* ptr, size_t allocated_size,
+      HookMemoryMutable is_mutable = HookMemoryMutable::kMutable) {
+    return {MallocHookRecorder::kDelete,  ptr, 0, allocated_size, is_mutable,
+            MallocHookRecorder::kTCMalloc};
+  }
+
+  std::vector<MallocHookRecorder::CallEntry> Log(bool include_mmap) {
+    return recorder_.ConsumeLog(include_mmap);
+  }
+  void RestartLog() { recorder_.Restart(); }
+
+  size_t GetAllocatedSize(const void* ptr) {
+    return MallocExtension::GetAllocatedSize(ptr).value();
+  }
+
+ private:
+  MallocHookRecorder recorder_;
+};
+
+TEST_F(TCMallocHookTest, MallocInvokesHook) {
+  void* p1 = malloc(10);
+  EXPECT_THAT(Log(false), ElementsAre(NewCall(p1, 10, GetAllocatedSize(p1))));
+  free(p1);
+}
+
+TEST_F(TCMallocHookTest, FreeInvokesHook) {
+  void* p1 = malloc(15);
+  size_t allocated_size = GetAllocatedSize(p1);
+  // Copy pointers to suppress use-after-free warnings.
+  void* copy_p1 = p1;
+  benchmark::DoNotOptimize(copy_p1);
+  free(p1);
+  EXPECT_THAT(Log(false), ElementsAre(NewCall(copy_p1, 15, allocated_size),
+                                      DeleteCall(copy_p1, allocated_size)));
+}
+
+TEST_F(TCMallocHookTest, CallocInvokesHook) {
+  void* p1 = calloc(13, 2);
+  EXPECT_THAT(Log(false),
+              ElementsAre(NewCall(p1, 13 * 2, GetAllocatedSize(p1))));
+  free(p1);
+}
+
+TEST_F(TCMallocHookTest, ReallocInvokesHook) {
+  void* p1 = calloc(1, 1);
+  size_t p1_allocated_size = GetAllocatedSize(p1);
+  RestartLog();
+  // Copy pointers to suppress use-after-free warnings.
+  void* copy_p1 = p1;
+  benchmark::DoNotOptimize(copy_p1);
+  void* p2 = realloc(p1, 30000);
+  EXPECT_THAT(Log(false), ElementsAre(NewCall(p2, 30000, GetAllocatedSize(p2)),
+                                      DeleteCall(copy_p1, p1_allocated_size)));
+  free(p2);
+}
+
+TEST_F(TCMallocHookTest, ReallocShrinkInvokesHook) {
+  void* p1 = calloc(1000, 1);
+  size_t p1_allocated_size = GetAllocatedSize(p1);
+  RestartLog();
+  // Copy pointers to suppress use-after-free warnings.
+  void* copy_p1 = p1;
+  benchmark::DoNotOptimize(copy_p1);
+  void* p2 = realloc(p1, 100);
+  EXPECT_THAT(Log(false), ElementsAre(NewCall(p2, 100, GetAllocatedSize(p2)),
+                                      DeleteCall(copy_p1, p1_allocated_size)));
+  free(p2);
+}
+
+TEST_F(TCMallocHookTest, ReallocPopInvokesHook) {
+  void* p1 = calloc(1000, 1);
+  size_t p1_allocated_size = GetAllocatedSize(p1);
+  RestartLog();
+  // Copy pointers to suppress use-after-free warnings.
+  void* copy_p1 = p1;
+  benchmark::DoNotOptimize(copy_p1);
+  void* p2 = realloc(p1, 999);
+  EXPECT_THAT(
+      Log(false),
+      testing::AnyOf(ElementsAre(DeleteCall(copy_p1, p1_allocated_size,
+                                            HookMemoryMutable::kImmutable),
+                                 NewCall(p2, 999, GetAllocatedSize(p2),
+                                         HookMemoryMutable::kImmutable)),
+
+                     ElementsAre(NewCall(p2, 999, GetAllocatedSize(p2),
+                                         HookMemoryMutable::kMutable),
+                                 DeleteCall(copy_p1, p1_allocated_size,
+                                            HookMemoryMutable::kMutable))));
+  free(p2);
+}
+
+TEST_F(TCMallocHookTest, BoundedReallocInvokesHook) {
+  // Triggers the new_size < lower_bound_to_grow case in realloc
+  void* p1 = calloc(8000, 1);
+  size_t p1_allocated_size = GetAllocatedSize(p1);
+  RestartLog();
+  // Copy pointers to suppress use-after-free warnings.
+  void* copy_p1 = p1;
+  benchmark::DoNotOptimize(copy_p1);
+  void* p2 = realloc(p1, 9000);
+  EXPECT_THAT(Log(false), ElementsAre(NewCall(p2, 9000, GetAllocatedSize(p2)),
+                                      DeleteCall(copy_p1, p1_allocated_size)));
+  free(p2);
+}
+
+TEST_F(TCMallocHookTest, StrDupInvokesHook) {
+  char* p1 = strdup("test");
+  EXPECT_THAT(Log(false), ElementsAre(NewCall(p1, 5, GetAllocatedSize(p1))));
+  free(p1);
+}
+
+TEST_F(TCMallocHookTest, PosixMemalignInvokesHook) {
+  void* p1;
+  ASSERT_EQ(posix_memalign(&p1, sizeof(p1), 40), 0);
+  EXPECT_THAT(Log(false), ElementsAre(NewCall(p1, 40, GetAllocatedSize(p1))));
+  free(p1);
+}
+
+TEST_F(TCMallocHookTest, MemalignInvokesHook) {
+  void* p1 = memalign(sizeof(p1) * 2, 50);
+  EXPECT_THAT(Log(false), ElementsAre(NewCall(p1, 50, GetAllocatedSize(p1))));
+  free(p1);
+}
+
+TEST_F(TCMallocHookTest, VallocInvokesHook) {
+  void* p1 = valloc(60);
+  EXPECT_THAT(Log(false), ElementsAre(NewCall(p1, 60, GetAllocatedSize(p1))));
+  free(p1);
+}
+
+#if defined(__BIONIC__) || defined(__GLIBC__) || defined(__NEWLIB__)
+TEST_F(TCMallocHookTest, PvallocInvokesHook) {
+  uint64_t pagesize = sysconf(_SC_PAGESIZE);
+  void* p1 = pvalloc(70);
+  EXPECT_THAT(Log(false),
+              ElementsAre(NewCall(p1, pagesize, GetAllocatedSize(p1))));
+  free(p1);
+}
+#endif
+
+TEST_F(TCMallocHookTest, OperatorNewInvokesHook) {
+  void* p1 = ::operator new(11);
+  EXPECT_THAT(Log(false), ElementsAre(NewCall(p1, 11, GetAllocatedSize(p1))));
+  ::operator delete(p1);
+}
+
+TEST_F(TCMallocHookTest, OperatorNewArrayInvokesHook) {
+  void* p1 = ::operator new[](13);
+  EXPECT_THAT(Log(false), ElementsAre(NewCall(p1, 13, GetAllocatedSize(p1))));
+  ::operator delete[](p1);
+}
+
+TEST_F(TCMallocHookTest, NothrowOperatorNewInvokesHook) {
+  void* p1 = ::operator new(11, std::nothrow);
+  EXPECT_THAT(Log(false), ElementsAre(NewCall(p1, 11, GetAllocatedSize(p1))));
+  ::operator delete(p1);
+}
+
+TEST_F(TCMallocHookTest, NothrowOperatorNewArrayInvokesHook) {
+  void* p1 = ::operator new[](13, std::nothrow);
+  EXPECT_THAT(Log(false), ElementsAre(NewCall(p1, 13, GetAllocatedSize(p1))));
+  ::operator delete[](p1);
+}
+
+TEST_F(TCMallocHookTest, OperatorDeleteInvokesHook) {
+  void* p1 = ::operator new(11);
+  size_t allocated_size = GetAllocatedSize(p1);
+  RestartLog();
+  // Copy pointers to suppress use-after-free warnings.
+  void* copy_p1 = p1;
+  benchmark::DoNotOptimize(copy_p1);
+  ::operator delete(p1);
+  EXPECT_THAT(Log(false), ElementsAre(DeleteCall(copy_p1, allocated_size)));
+}
+
+TEST_F(TCMallocHookTest, SizedOperatorDeleteInvokesHook) {
+  void* p1 = ::operator new(11);
+  size_t allocated_size = GetAllocatedSize(p1);
+  RestartLog();
+  // Copy pointers to suppress use-after-free warnings.
+  void* copy_p1 = p1;
+  benchmark::DoNotOptimize(copy_p1);
+  sized_delete(p1, 11);
+  EXPECT_THAT(Log(false), ElementsAre(DeleteCall(copy_p1, allocated_size)));
+}
+
+TEST_F(TCMallocHookTest, OperatorDeleteArrayInvokesHook) {
+  void* p1 = ::operator new[](13);
+  size_t allocated_size = GetAllocatedSize(p1);
+  RestartLog();
+  // Copy pointers to suppress use-after-free warnings.
+  void* copy_p1 = p1;
+  benchmark::DoNotOptimize(copy_p1);
+  ::operator delete[](p1);
+  EXPECT_THAT(Log(false), ElementsAre(DeleteCall(copy_p1, allocated_size)));
+}
+
+TEST_F(TCMallocHookTest, NothrowOperatorDeleteInvokesHook) {
+  void* p1 = ::operator new(11);
+  size_t allocated_size = GetAllocatedSize(p1);
+  RestartLog();
+  // Copy pointers to suppress use-after-free warnings.
+  void* copy_p1 = p1;
+  benchmark::DoNotOptimize(copy_p1);
+  ::operator delete(p1, std::nothrow);
+  EXPECT_THAT(Log(false), ElementsAre(DeleteCall(copy_p1, allocated_size)));
+}
+
+TEST_F(TCMallocHookTest, NothrowOperatorDeleteArrayInvokesHook) {
+  void* p1 = ::operator new[](13);
+  size_t allocated_size = GetAllocatedSize(p1);
+  RestartLog();
+  // Copy pointers to suppress use-after-free warnings.
+  void* copy_p1 = p1;
+  benchmark::DoNotOptimize(copy_p1);
+  ::operator delete[](p1, std::nothrow);
+  EXPECT_THAT(Log(false), ElementsAre(DeleteCall(copy_p1, allocated_size)));
+}
+
+TEST_F(TCMallocHookTest, SizeReturningOperatorNewInvokesHook) {
+  sized_ptr_t res = __size_returning_new(29);
+  EXPECT_THAT(Log(false),
+              ElementsAre(NewCall(res.p, res.n, GetAllocatedSize(res.p))));
+  ::operator delete(res.p);
+}
+
+TEST_F(TCMallocHookTest, GuardedAllocatedSizeIsRequested) {
+  MallocExtension::ActivateGuardedSampling();
+  MallocExtension::SetProfileSamplingInterval(1);
+  // Allocate a large block of memory to make sure
+  // Sampler::RecordAllocationSlow is called so that
+  // Sampler::bytes_until_sample_ is updated.
+  void* p = ::operator new[](256 * 1024 * 1024);
+  ::operator delete[](p);
+
+  MallocExtension::SetGuardedSamplingInterval(0);  // Guard every sample
+  // Allocate some memory until allocs_until_guarded_sample_ reaches 0.
+  for (int i = 0; i < 1000; i++) {
+    void* p = ::operator new[](32);
+    ::operator delete[](p);
+    RestartLog();  // Prevent overflowing log
+  }
+
+  sized_ptr_t res = __size_returning_new(34);
+  EXPECT_THAT(Log(false), ElementsAre(NewCall(res.p, 34, 34)));
+  EXPECT_EQ(GetAllocatedSize(res.p), 34);
+  EXPECT_EQ(res.n, 34);
+
+  ::operator delete(res.p);
+  MallocExtension::SetProfileSamplingInterval(0);
+}
 
 // These are used as callbacks by the sanity-check.  Set* and Reset* register
 // the hook that counts how many times the associated memory function is called.
@@ -63,25 +332,25 @@ using testing::SizeIs;
     ASSERT_TRUE(MallocHook::Remove##hook_type(IncrementCallsTo##hook_type)); \
   }
 
-#define MAKE_HOOK_CALLBACK_ABSL(hook_type, ...)                     \
-  static int g_##hook_type##_calls = 0;                             \
-  static void IncrementCallsTo##hook_type(__VA_ARGS__) {            \
-    g_##hook_type##_calls++;                                        \
-  }                                                                 \
-  static void Verify##hook_type##WasCalled() {                      \
-    ASSERT_GT(g_##hook_type##_calls, 0);                            \
-    g_##hook_type##_calls = 0; /* reset for next call */            \
-  }                                                                 \
-  static void Set##hook_type() {                                    \
-    ASSERT_TRUE(absl::base_internal::MallocHook::Add##hook_type(    \
-        IncrementCallsTo##hook_type));                              \
-  }                                                                 \
-  static void Reset##hook_type() {                                  \
-    ASSERT_TRUE(absl::base_internal::MallocHook::Remove##hook_type( \
-        IncrementCallsTo##hook_type));                              \
+#define MAKE_HOOK_CALLBACK_ABSL(hook_type, ...)                              \
+  static int g_##hook_type##_calls = 0;                                      \
+  static void IncrementCallsTo##hook_type(__VA_ARGS__) {                     \
+    g_##hook_type##_calls++;                                                 \
+  }                                                                          \
+  static void Verify##hook_type##WasCalled() {                               \
+    ASSERT_GT(g_##hook_type##_calls, 0);                                     \
+    g_##hook_type##_calls = 0; /* reset for next call */                     \
+  }                                                                          \
+  static void Set##hook_type() {                                             \
+    ASSERT_TRUE(MallocHook::Add##hook_type(IncrementCallsTo##hook_type));    \
+  }                                                                          \
+  static void Reset##hook_type() {                                           \
+    ASSERT_TRUE(MallocHook::Remove##hook_type(IncrementCallsTo##hook_type)); \
   }
 
 // We do one for each hook typedef in malloc_hook.h
+MAKE_HOOK_CALLBACK_ABSL(NewHook, const MallocHook::NewInfo&);
+MAKE_HOOK_CALLBACK_ABSL(DeleteHook, const MallocHook::DeleteInfo&);
 MAKE_HOOK_CALLBACK(SampledNewHook, const MallocHook::SampledAlloc&);
 MAKE_HOOK_CALLBACK(SampledDeleteHook,
                    const
@@ -95,7 +364,10 @@ TEST(TCMallocTest, GetStatsReportsHooks) {
   int new_hook_count = 0;
   {
     tcmalloc_internal::AllocationGuard guard;
+    new_hook_count = tcmalloc_internal::new_hooks_.size();
   }
+  EXPECT_GT(new_hook_count, 0);
+  GTEST_SKIP() << "Under debug builds, AllocationGuard installs extra hooks.";
 #endif
 
   std::string stats;
@@ -116,6 +388,33 @@ TEST(TCMallocTest, GetStatsReportsHooks) {
   EXPECT_THAT(
       stats,
       HasSubstr("MALLOC HOOKS: NEW=0 DELETE=0 SAMPLED_NEW=0 SAMPLED_DELETE=0"));
+
+  // Add a hook, confirm it's reported, add a second, then remove them.
+  SetNewHook();
+  stats = MallocExtension::GetStats();
+  EXPECT_THAT(
+      stats,
+      HasSubstr("MALLOC HOOKS: NEW=1 DELETE=0 SAMPLED_NEW=0 SAMPLED_DELETE=0"));
+  SetNewHook();
+  stats = MallocExtension::GetStats();
+  EXPECT_THAT(
+      stats,
+      HasSubstr("MALLOC HOOKS: NEW=2 DELETE=0 SAMPLED_NEW=0 SAMPLED_DELETE=0"));
+  ResetNewHook();
+  ResetNewHook();
+
+  SetDeleteHook();
+  stats = MallocExtension::GetStats();
+  EXPECT_THAT(
+      stats,
+      HasSubstr("MALLOC HOOKS: NEW=0 DELETE=1 SAMPLED_NEW=0 SAMPLED_DELETE=0"));
+  SetDeleteHook();
+  stats = MallocExtension::GetStats();
+  EXPECT_THAT(
+      stats,
+      HasSubstr("MALLOC HOOKS: NEW=0 DELETE=2 SAMPLED_NEW=0 SAMPLED_DELETE=0"));
+  ResetDeleteHook();
+  ResetDeleteHook();
 
   SetSampledNewHook();
   stats = MallocExtension::GetStats();
@@ -142,6 +441,29 @@ TEST(TCMallocTest, GetStatsReportsHooks) {
       HasSubstr("MALLOC HOOKS: NEW=0 DELETE=0 SAMPLED_NEW=0 SAMPLED_DELETE=2"));
   ResetSampledDeleteHook();
   ResetSampledDeleteHook();
+}
+
+TEST(TCMallocTest, MarkThreadBusy) {
+  static thread_local bool log_hooks = false;
+
+  // Verify that MarkThreadBusy does not cause the hooks to be invoked.  See
+  // b/112640764.
+  const auto new_hook = [](const MallocHook::NewInfo&) {
+    TC_CHECK(!log_hooks);
+  };
+  const auto delete_hook = [](const MallocHook::DeleteInfo&) {
+    TC_CHECK(!log_hooks);
+  };
+
+  log_hooks = true;
+  ASSERT_TRUE(MallocHook::AddNewHook(new_hook));
+  ASSERT_TRUE(MallocHook::AddDeleteHook(delete_hook));
+
+  MallocExtension::MarkThreadBusy();
+
+  ASSERT_TRUE(MallocHook::RemoveNewHook(new_hook));
+  ASSERT_TRUE(MallocHook::RemoveDeleteHook(delete_hook));
+  log_hooks = false;
 }
 
 }  // namespace
---
+++ tcmalloc/testing/malloc_hook_recorder.cc
@@ -0,0 +1,262 @@
+// Copyright 2025 The TCMalloc Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "tcmalloc/testing/malloc_hook_recorder.h"
+
+#include <cstddef>
+
+#include "absl/base/optimization.h"
+
+#define _GNU_SOURCE 1  // for mremap (in sys/mman.h)
+#include <stdint.h>
+#include <sys/mman.h>
+
+#include <ostream>
+#include <vector>
+
+#include "absl/base/attributes.h"
+#include "absl/base/macros.h"
+#include "absl/debugging/stacktrace.h"
+#include "tcmalloc/internal/logging.h"
+
+ABSL_DEFINE_ATTRIBUTE_SECTION_VARS(google_malloc);
+ABSL_DECLARE_ATTRIBUTE_SECTION_VARS(google_malloc);
+
+#define ADDR_IN_ATTRIBUTE_SECTION(addr, name)                         \
+  (reinterpret_cast<uintptr_t>(ABSL_ATTRIBUTE_SECTION_START(name)) <= \
+       reinterpret_cast<uintptr_t>(addr) &&                           \
+   reinterpret_cast<uintptr_t>(addr) <                                \
+       reinterpret_cast<uintptr_t>(ABSL_ATTRIBUTE_SECTION_STOP(name)))
+
+namespace tcmalloc {
+namespace tcmalloc_testing {
+
+static bool CheckSectionVars() {
+#if defined(__clang__)
+  static bool ok = [] {
+    ABSL_INIT_ATTRIBUTE_SECTION_VARS(google_malloc);
+    if (ABSL_ATTRIBUTE_SECTION_START(google_malloc) ==
+        ABSL_ATTRIBUTE_SECTION_STOP(google_malloc)) {
+      TC_LOG(
+          "google_malloc section is missing, no caller information available");
+      return false;
+    }
+    return true;
+  }();
+  return ok;
+#else
+  // GOOGLE_MALLOC_SECTION_BEGIN/GOOGLE_MALLOC_SECTION_END are no-ops on GCC.
+  return false;
+#endif
+}
+
+MallocHookRecorder::MallocHookRecorder(int log_size, bool overflow)
+    : overflow_(overflow) {
+  TC_CHECK_GE(log_size, 0);
+  log_.reserve(log_size);
+  TC_CHECK(!tls_hook_);
+  TC_CHECK(
+      tcmalloc::MallocHook::AddNewHook(&MallocHookRecorder::GlobalNewHook));
+  TC_CHECK(tcmalloc::MallocHook::AddDeleteHook(
+      &MallocHookRecorder::GlobalDeleteHook));
+  tls_hook_ = this;
+}
+
+MallocHookRecorder::~MallocHookRecorder() {
+  TC_CHECK_EQ(tls_hook_, this);
+  tls_hook_ = nullptr;
+  TC_CHECK(
+      tcmalloc::MallocHook::RemoveNewHook(&MallocHookRecorder::GlobalNewHook));
+  TC_CHECK(MallocHook::RemoveDeleteHook(&MallocHookRecorder::GlobalDeleteHook));
+}
+
+void MallocHookRecorder::Restart() {
+  enabled_ = false;
+  log_.clear();
+  head_ = 0;
+  enabled_ = true;
+}
+
+void MallocHookRecorder::Stop() { enabled_ = false; }
+
+static bool IsMMap(MallocHookRecorder::Type t) {
+  switch (t) {
+    case MallocHookRecorder::kNew:
+    case MallocHookRecorder::kDelete:
+      return false;
+    case MallocHookRecorder::kMmap:
+    case MallocHookRecorder::kMremap:
+    case MallocHookRecorder::kMunmap:
+    case MallocHookRecorder::kSbrk:
+      return true;
+  }
+
+  __builtin_unreachable();
+  return false;
+}
+
+std::vector<MallocHookRecorder::CallEntry> MallocHookRecorder::ConsumeLog(
+    bool include_mmap) {
+  Stop();
+  std::vector<CallEntry> log;
+  log.reserve(log_.size());
+  for (int i = head_; i < log_.size(); i++) {
+    if (include_mmap || !IsMMap(log_[i].type)) {
+      log.push_back(log_[i]);
+    }
+  }
+  for (int i = 0; i < head_; i++) {
+    if (include_mmap || !IsMMap(log_[i].type)) {
+      log.push_back(log_[i]);
+    }
+  }
+  log_.clear();
+  head_ = 0;
+  return log;
+}
+
+void MallocHookRecorder::AddLog(CallEntry entry) {
+  if (CheckSectionVars()) {
+    entry.caller = kOther;
+    void* stack[32];
+    int depth = absl::GetStackTrace(stack, ABSL_ARRAYSIZE(stack), 3);
+    for (int i = depth - 1; i >= 0; --i) {
+      if (ADDR_IN_ATTRIBUTE_SECTION(stack[i], google_malloc)) {
+        entry.caller = kTCMalloc;
+        break;
+      }
+    }
+  } else {
+    // GCC doesn't support
+    // GOOGLE_MALLOC_SECTION_BEGIN/GOOGLE_MALLOC_SECTION_END.  Presume calls
+    // came from TCMalloc.
+#if defined(__clang__)
+    entry.caller = kError;
+#else
+    entry.caller = kTCMalloc;
+#endif
+  }
+
+  if (log_.size() < log_.capacity()) {
+    log_.push_back(entry);
+  } else {
+    TC_CHECK(overflow_);
+    log_[head_] = entry;
+    if (++head_ == log_.size()) head_ = 0;
+  }
+}
+
+void MallocHookRecorder::GlobalNewHook(const MallocHook::NewInfo& info) {
+  if (tls_hook_ && tls_hook_->enabled_) {
+    tls_hook_->AddLog({kNew, info.ptr, info.requested_size, info.allocated_size,
+                       info.is_mutable});
+  }
+}
+
+void MallocHookRecorder::GlobalDeleteHook(const MallocHook::DeleteInfo& info) {
+  if (tls_hook_ && tls_hook_->enabled_) {
+    tls_hook_->AddLog(
+        {kDelete, info.ptr, 0, info.allocated_size, info.is_mutable});
+  }
+}
+
+thread_local MallocHookRecorder* MallocHookRecorder::tls_hook_ = nullptr;
+
+bool operator==(const MallocHookRecorder::CallEntry& lhs,
+                const MallocHookRecorder::CallEntry& rhs) {
+  if (lhs.type != rhs.type) return false;
+  if (lhs.ptr != rhs.ptr || lhs.requested_size != rhs.requested_size ||
+      lhs.allocated_size != rhs.allocated_size ||
+      lhs.is_mutable != rhs.is_mutable) {
+    return false;
+  }
+  if (lhs.caller != MallocHookRecorder::kAny &&
+      rhs.caller != MallocHookRecorder::kAny && lhs.caller != rhs.caller) {
+    return false;
+  }
+  if (lhs.type == MallocHookRecorder::kMmap) {
+    if (lhs.address != rhs.address || lhs.protection != rhs.protection ||
+        lhs.flags != rhs.flags || lhs.fd != rhs.fd ||
+        lhs.offset != rhs.offset) {
+      return false;
+    }
+  }
+  if (lhs.type == MallocHookRecorder::kMremap) {
+    if (lhs.address != rhs.address || lhs.flags != rhs.flags ||
+        lhs.old_size != rhs.old_size || lhs.new_address != rhs.new_address) {
+      return false;
+    }
+  }
+  return true;
+}
+
+static const char* ToString(MallocHookRecorder::Caller caller) {
+  switch (caller) {
+    case MallocHookRecorder::kError:
+      return "<error>";
+    case MallocHookRecorder::kOther:
+      return "other";
+    case MallocHookRecorder::kAny:
+      return "";
+    case MallocHookRecorder::kTCMalloc:
+      return "tcmalloc";
+  }
+  ABSL_UNREACHABLE();
+}
+
+static const char* ToString(HookMemoryMutable is_mutable) {
+  switch (is_mutable) {
+    case HookMemoryMutable::kMutable:
+      return "Mutable";
+    case HookMemoryMutable::kImmutable:
+      return "Immutable";
+  }
+  ABSL_UNREACHABLE();
+}
+
+std::ostream& operator<<(std::ostream& stream,
+                         const MallocHookRecorder::CallEntry& lhs) {
+  switch (lhs.type) {
+    case MallocHookRecorder::kNew:
+      return stream << ToString(lhs.caller) << "::New(" << lhs.ptr << ", "
+                    << lhs.requested_size << ", " << lhs.allocated_size << ", "
+                    << ToString(lhs.is_mutable) << ")";
+    case MallocHookRecorder::kDelete:
+      return stream << ToString(lhs.caller) << "::Delete(" << lhs.ptr << ", "
+                    << lhs.allocated_size << ", " << ToString(lhs.is_mutable)
+                    << ")";
+    case MallocHookRecorder::kMmap:
+      return stream << ToString(lhs.caller) << "::Mmap(" << lhs.ptr << ", "
+                    << lhs.address << ", " << lhs.requested_size << ", "
+                    << lhs.protection << ", " << lhs.flags << ", " << lhs.fd
+                    << ", " << lhs.offset << ")";
+    case MallocHookRecorder::kMremap:
+      return stream << ToString(lhs.caller) << "::Mremap(" << lhs.ptr << ", "
+                    << lhs.address << ", " << lhs.old_size << ", "
+                    << lhs.requested_size << ", " << lhs.flags << ", "
+                    << lhs.new_address << ")";
+    case MallocHookRecorder::kMunmap:
+      return stream << ToString(lhs.caller) << "::Munmap(" << lhs.ptr << ", "
+                    << lhs.requested_size << ")";
+    case MallocHookRecorder::kSbrk:
+      return stream << ToString(lhs.caller) << "::Sbrk(" << lhs.ptr << ", "
+                    << static_cast<ptrdiff_t>(lhs.requested_size) << ")";
+  }
+  ABSL_UNREACHABLE();
+}
+
+#undef ADDR_IN_ATTRIBUTE_SECTION
+
+}  // namespace tcmalloc_testing
+}  // namespace tcmalloc
