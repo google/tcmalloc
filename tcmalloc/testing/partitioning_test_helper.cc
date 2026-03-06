d745ae97b13d5bc248bac40382c69386386ee310
Loris Reiff <liblor@google.com>
2026-03-06 07:18:34 -0800

    Improve heap partitioning enablement sanity test.
    PiperOrigin-RevId: 879622711
    Change-Id: I312e4e7a71d414d17c62eba3fac991dc15fb1715

--- tcmalloc/testing/partitioning_test_helper.cc
+++ tcmalloc/testing/partitioning_test_helper.cc
@@ -1,4 +1,4 @@
-// Copyright 2019 The TCMalloc Authors
+// Copyright 2026 The TCMalloc Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,47 +11,41 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#include <cstddef>
-#include <cstdlib>
-#include <string>
 
-#include "gtest/gtest.h"
+#include <cstdlib>
+
+#include "absl/strings/str_format.h"
+#include "absl/strings/string_view.h"
 #include "tcmalloc/internal/memory_tag.h"
 #include "tcmalloc/internal/parameter_accessors.h"
-#include "tcmalloc/testing/testutil.h"
-
-namespace tcmalloc {
-namespace {
+#include "tcmalloc/malloc_extension.h"
 
 using ::tcmalloc::tcmalloc_internal::GetMemoryTag;
-using ::tcmalloc::tcmalloc_internal::MemoryTag;
+using ::tcmalloc::tcmalloc_internal::MemoryTagToLabel;
 
 extern "C" {
 void* __alloc_token_0_malloc(size_t) noexcept;
 void* __alloc_token_1_malloc(size_t) noexcept;
 }
 
-TEST(Partition, AssignedCorrectly) {
-  ScopedNeverSample never_sample;
+int main() {
   void* ptr_0 = __alloc_token_0_malloc(8);
   void* ptr_1 = __alloc_token_1_malloc(8);
 
-  EXPECT_EQ(GetMemoryTag(ptr_0), MemoryTag::kNormalP0);
-  if (TCMalloc_Internal_GetHeapPartitioning()) {
-    ASSERT_NE(MemoryTag::kNormalP0, MemoryTag::kNormalP1);
-    EXPECT_EQ(GetMemoryTag(ptr_1), MemoryTag::kNormalP1);
-    char* heap_partitioning_env = std::getenv("TCMALLOC_HEAP_PARTITIONING");
-    std::string heap_partitioning_env_str =
-        heap_partitioning_env == nullptr ? "true" : heap_partitioning_env;
-    EXPECT_NE(heap_partitioning_env, "false");
-    EXPECT_NE(heap_partitioning_env, "0");
-  } else {
-    EXPECT_EQ(GetMemoryTag(ptr_1), MemoryTag::kNormal);
-  }
+  bool heap_partitioning = TCMalloc_Internal_GetHeapPartitioning();
+  bool security_partitioning = tcmalloc::MallocExtension::GetNumericProperty(
+                                   "tcmalloc.security_partitioning_active")
+                                   .value_or(0);
+
+  absl::string_view tag_0 = MemoryTagToLabel(GetMemoryTag(ptr_0));
+  absl::string_view tag_1 = MemoryTagToLabel(GetMemoryTag(ptr_1));
+
+  absl::PrintF("heap_partitioning:%d\n", heap_partitioning);
+  absl::PrintF("security_partitioning:%d\n", security_partitioning);
+  absl::PrintF("ptr_0_tag:%s\n", tag_0);
+  absl::PrintF("ptr_1_tag:%s\n", tag_1);
 
   free(ptr_0);
   free(ptr_1);
+  return 0;
 }
-
-}  // namespace
-}  // namespace tcmalloc
