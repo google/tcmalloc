8ca4872278975e83a41f7385f4975f387067e567
Chris Kennelly <ckennelly@google.com>
2026-07-17 16:52:21 -0700

    Adjust limits dynamically in startup_size_test based on active features to prevent flakiness.
    PiperOrigin-RevId: 949826060

--- tcmalloc/testing/startup_size_test.cc
+++ tcmalloc/testing/startup_size_test.cc
@@ -51,17 +51,53 @@ TEST(StartupSizeTest, Basic) {
   const auto map = MallocExtension::GetProperties();
   ASSERT_NE(map.count("tcmalloc.metadata_bytes"), 0)
       << "couldn't run - no tcmalloc data. Check your malloc configuration.";
-  size_t percpu = Property(map, "tcmalloc.cpu_free");
+  bool percpu_active = false;
+  const auto percpu_active_iter = map.find("tcmalloc.per_cpu_caches_active");
+  if (percpu_active_iter != map.end() && percpu_active_iter->second.value > 0) {
+    percpu_active = true;
+  }
+
+  bool partitioning_active = false;
+  const auto partitioning_active_iter =
+      map.find("tcmalloc.security_partitioning_active");
+  if (partitioning_active_iter != map.end() &&
+      partitioning_active_iter->second.value > 0) {
+    partitioning_active = true;
+  }
+
+  bool sharded_cache_active = false;
+  const auto sharded_cache_active_iter =
+      map.find("tcmalloc.experiment.TEST_ONLY_TCMALLOC_SHARDED_TRANSFER_CACHE");
+  if (sharded_cache_active_iter != map.end() &&
+      sharded_cache_active_iter->second.value > 0) {
+    sharded_cache_active = true;
+  }
+
 #ifdef __powerpc64__
   size_t metadata_limit = 36.5 * MiB;
 #else
   size_t metadata_limit = 28 * MiB;
 #endif
+
+  if (partitioning_active) {
+    // Extra HugePageAwareAllocator (~815 KiB) and some slots
+    metadata_limit += 1 * MiB;
+  }
+
   // Check whether per-cpu is active
   size_t upper_percpu_limit = 0;
-  if (percpu > 0) {
-    // Account for 16KiB per cpu slab
-    upper_percpu_limit = tcmalloc_internal::NumCPUs() * 32 * 1024;
+  if (percpu_active) {
+    // Slabs: 32 KiB per CPU.
+    // ResizeInfo: ~5.3 KiB per CPU.
+    size_t bytes_per_cpu = 32 * 1024 + 5500;
+
+    if (sharded_cache_active) {
+      // Shards: up to NumCPUs / 8 shards, each ~320 KiB.
+      // So on average ~40 KiB per CPU.
+      bytes_per_cpu += 40 * 1024;
+    }
+
+    upper_percpu_limit = tcmalloc_internal::NumCPUs() * bytes_per_cpu;
   }
   size_t meta = Property(map, "tcmalloc.metadata_bytes");
   size_t physical = Property(map, "generic.physical_memory_used");
