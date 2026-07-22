6bdb7864c37979000197d72b518e83407af4551a
Chris Kennelly <ckennelly@google.com>
2026-07-22 09:21:42 -0700

    Bolster cross-checks in memory stats.
    PiperOrigin-RevId: 952163319

--- tcmalloc/internal/memory_stats_test.cc
+++ tcmalloc/internal/memory_stats_test.cc
@@ -33,8 +33,11 @@ TEST(Stats, ValidRanges) {
 #endif
 
   EXPECT_GT(stats.vss, 0);
+  EXPECT_GE(stats.vss, stats.rss);
   EXPECT_GT(stats.rss, 0);
+  EXPECT_GE(stats.rss, stats.shared);
   EXPECT_GT(stats.shared, 0);
+  EXPECT_GE(stats.vss, stats.code + stats.data);
   EXPECT_GT(stats.code, 0);
   EXPECT_GT(stats.data, 0);
 }
