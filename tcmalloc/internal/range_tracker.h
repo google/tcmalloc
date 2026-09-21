6473491e69b11d41cc6d0c47e02838b5309912c8
Chris Kennelly CA <ckennelly@google.com>
2026-09-21 12:48:36 -0700

    Add `PrevFreeRange` to `Bitmap` and `RangeTracker`.
    Add `Bitmap::PrevFreeRange` and `RangeTracker::PrevFreeRange` to locate and iterate backwards over clear bit ranges using word-level `FindClearBackwards` and `FindSetBackwards` scans.
    
    PiperOrigin-RevId: 985451226

--- tcmalloc/internal/range_tracker.h
+++ tcmalloc/internal/range_tracker.h
@@ -104,6 +104,14 @@ class Bitmap {
   // put it in *index, *length and return true; else return false.
   bool NextFreeRange(size_t start, size_t* index, size_t* length) const;
 
+  // If there is at least one free range before and exclusive of <end>,
+  // put it in *index, *length and return true; else return false.
+  // To iterate backwards through all free ranges:
+  //   size_t index = bitmap.size(), n;
+  //   while (bitmap.PrevFreeRange(index, &index, &n)) { ... }
+  [[nodiscard]] bool PrevFreeRange(size_t end, size_t* index,
+                                   size_t* length) const;
+
   // Returns index of the first {true, false} bit >= index, or N if none.
   size_t FindSet(size_t index) const;
   size_t FindClear(size_t index) const;
@@ -188,6 +196,14 @@ class RangeTracker {
   // If there is at least one free range at or after <start>,
   // put it in *index, *length and return true; else return false.
   bool NextFreeRange(size_t start, size_t* index, size_t* length) const;
+
+  // If there is at least one free range before and exclusive of <end>,
+  // put it in *index, *length and return true; else return false.
+  // To iterate backwards through all free ranges:
+  //   size_t index = tracker.size(), n;
+  //   while (tracker.PrevFreeRange(index, &index, &n)) { ... }
+  [[nodiscard]] bool PrevFreeRange(size_t end, size_t* index,
+                                   size_t* length) const;
 
   void Clear();
 
@@ -353,6 +369,14 @@ inline bool RangeTracker<N>::NextFreeRange(size_t start, size_t* index,
   return bits_.NextFreeRange(start, index, length);
 }
 
+// If there is at least one free range before <end>,
+// put it in *index, *length and return true; else return false.
+template <size_t N>
+inline bool RangeTracker<N>::PrevFreeRange(size_t end, size_t* index,
+                                           size_t* length) const {
+  return bits_.PrevFreeRange(end, index, length);
+}
+
 template <size_t N>
 inline void RangeTracker<N>::Clear() {
   bits_.Clear();
@@ -513,6 +537,19 @@ inline bool Bitmap<N>::NextFreeRange(size_t start, size_t* index,
   if (i == N) return false;
   size_t j = FindSet(i);
   *index = i;
+  *length = j - i;
+  return true;
+}
+
+template <size_t N>
+inline bool Bitmap<N>::PrevFreeRange(size_t end, size_t* index,
+                                     size_t* length) const {
+  if (end == 0) return false;
+  if (end > N) end = N;
+  ssize_t j = FindClearBackwards(end - 1);
+  if (j < 0) return false;
+  ssize_t i = FindSetBackwards(j);
+  *index = i + 1;
   *length = j - i;
   return true;
 }
